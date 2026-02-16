7363730d64451f23492d14d4e72eeb08aa4a7c1d
Vijay Solanki <141487233+vijaysolanki9079@users.noreply.github.com>
2026-02-16 13:44:43 +0530

    Change import from tensorflow to keras

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pickle
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+
 
 # Page config
 st.set_page_config(
